e70489ff71de3e0ea132c7f2ed1c809628543fbd
Dimo Mitev <104933623+dimosoftuni@users.noreply.github.com>
2026-05-19 21:52:00 +0300

    Overload add method to include three parameters

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,8 @@
 public class Calculator {
 
     // Addition
-    public int add(int a, int b) {
-        return a + b;
+    public int add(int a, int b, int c) {
+        return a + b + c;
     }
 
     // Subtraction
