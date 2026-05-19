5056bb8b05a56e7168bbaaebea83d03242cf0c2c
Dimo Mitev <dimosoftuni@gmail.com>
2026-05-19 21:54:42 +0300

    update one number

--- Calculator.java
+++ Calculator.java
@@ -26,7 +26,7 @@ public class Calculator {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
 
-        System.out.println("Add: " + calculator.add(10, 5));
+        System.out.println("Add: " + calculator.add(10, 50));
         System.out.println("Subtract: " + calculator.subtract(10, 5));
         System.out.println("Multiply: " + calculator.multiply(10, 5));
         System.out.println("Divide: " + calculator.divide(10, 5));
