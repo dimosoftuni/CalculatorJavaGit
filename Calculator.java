a07c60a3979051ced0a20e3f74f37214a1ee4219
Dimo Mitev <dimosoftuni@gmail.com>
2026-05-19 21:50:16 +0300

    Repo init

---
+++ Calculator.java
@@ -0,0 +1,34 @@
+public class Calculator {
+
+    // Addition
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Subtraction
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    // Multiplication
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // Division
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+        return (double) a / b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        System.out.println("Add: " + calculator.add(10, 5));
+        System.out.println("Subtract: " + calculator.subtract(10, 5));
+        System.out.println("Multiply: " + calculator.multiply(10, 5));
+        System.out.println("Divide: " + calculator.divide(10, 5));
+    }
+}
